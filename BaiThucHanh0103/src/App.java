b90d0d49eb6c191c182295827ed4d585e0b0c6c2
PhanDucLuong032211 <phanducluong032211@gmail.com>
2023-03-15 09:38:26 +0700

    commit note

--- BaiThucHanh0103/src/App.java
+++ BaiThucHanh0103/src/App.java
@@ -1,5 +1,32 @@
+import java.util.IntSummaryStatistics;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // tinh tong cac phan tu chang trong mang 2 chieu
+        //cac phan tu trong mang do nguoi dung nhap vao
+        Scanner sc = new Scanner(System.in);
+        int sohang, socot, tong = 0;
+        do
+        {
+            System.out.print("So hang = ");
+            sohang = sc.nextInt();
+        } while(sohang <= 0);
+        do
+        {
+            System.out.print("So cot = ");
+            socot = sc.nextInt();
+        } while(socot <= 0);
+        int arrA[][] = new int[sohang][socot];
+        for(int i = 0; i < sohang; i++)
+        {
+            for(int j =0; j<socot; j++)
+            {
+                System.out.printf("A[%d][%d] = ",i,j);
+                arrA[i][j] = sc.nextInt();
+                if(arrA[i][j] %2 == 0) tong+= arrA[i][j];
+            }
+        }
+        System.out.println("tong = " + tong);
     }
 }
