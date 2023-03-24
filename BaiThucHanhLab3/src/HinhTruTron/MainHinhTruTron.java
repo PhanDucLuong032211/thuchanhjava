c545f224fbf4004005386585378a4a1da8e15740
PhanDucLuong032211 <phanducluong032211@gmail.com>
2023-03-24 14:40:33 +0700

    commit note

---
+++ BaiThucHanhLab3/src/HinhTruTron/MainHinhTruTron.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        HinhTruTron htt= new HinhTruTron();
+        htt.nhapbkcc();
+        htt.tinhcv();
+        htt.tinhdt();
+        htt.tinhtt();
+        htt.inracv();
+        System.out.println(" ");
+        htt.inradt();
+        System.out.println(" ");
+        htt.inratt();
+    }
+}
